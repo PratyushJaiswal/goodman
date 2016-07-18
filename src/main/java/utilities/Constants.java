9119c8f3b19eb18807b1162040e29176743abd34
rais <rais@SSD-16-0035.corp.softwaysolutions.com>
2016-07-18 18:47:37 +0530

    Framework commit

---
+++ src/main/java/utilities/Constants.java
@@ -0,0 +1,8 @@
+package utilities;
+
+public class Constants {
+      public static final String URL = "URl of site you are going to automate";
+    
+      public static final String File_Path = System.getProperty("user.dir")+ "//path after the src folser (src//dataEngine)//name of excel sheet.xlsx";
+      public static final String File_Name = "name of excel sheet.xlsx";
+}
