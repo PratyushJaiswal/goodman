81c81dd55218f51aeddcaa45e342671cf9ba3303
rais <rais@SSD-16-0035.corp.softwaysolutions.com>
2016-07-18 19:48:51 +0530

    comit for the final work on monday 18th July 2016

--- src/main/java/frameworkClasses/frameworkClass.java
+++ src/main/java/frameworkClasses/frameworkClass.java
@@ -7,10 +7,13 @@ import org.apache.log4j.PropertyConfigurator;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
 
+import pageObjectFactory.homePageFactory;
 import utilities.Constants;
 
 public class frameworkClass {
@@ -18,6 +21,7 @@ public class frameworkClass {
 	private WebDriver driver;
 	private String sTestCaseName;
 	private int iTestCaseRow;
+	homePageFactory HP;
 	
 	@Parameters("browserType")
 	@BeforeClass
@@ -26,17 +30,25 @@ public class frameworkClass {
 		if(browser.equalsIgnoreCase("firefox")){
 			driver = new FirefoxDriver();
 		}else if(browser.equalsIgnoreCase("chrome")){
-			System.setProperty("webdriver.chrome.driver","C://Users//Pratyush//git//RFTTWithPAgeobjectFactory//driver//chromedriver.exe" );
+			System.setProperty("webdriver.chrome.driver",Constants.ChromeDriver_Path );
 			driver = new ChromeDriver();
 		}
-		driver.get(Constants.URL);
+		//driver.get(Constants.URL);
 		driver.manage().window().maximize();
-		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		
 		PropertyConfigurator.configure("src/utilities/log4j.properties");
 		driver.get(Constants.URL);
+		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+		HP = new homePageFactory(driver);
 		
-		
-	  }
+		}
+	@Test
+	public void clickAbout(){
+
+		HP.gotoAbout();
+		String actual = driver.getTitle();
+		Assert.assertEquals(actual, "Reliable Heating &amp; Cooling Systems From Goodman Brand");
+	}
 	
 	@AfterClass
 	  public void afterClass() {
--- src/main/java/utilities/Constants.java
+++ src/main/java/utilities/Constants.java
@@ -1,8 +1,9 @@
 package utilities;
 
 public class Constants {
-      public static final String URL = "URl of site you are going to automate";
+      public static final String URL = "http://www.goodmanmfg.com/";
     
       public static final String File_Path = System.getProperty("user.dir")+ "//path after the src folser (src//dataEngine)//name of excel sheet.xlsx";
       public static final String File_Name = "name of excel sheet.xlsx";
+      public static final String ChromeDriver_Path = System.getProperty("user.dir")+ "//src//main//java//utilities//chromedriver.exe";
 }
